c13931d9fb550fd2853cea03ebfb7836d5c18fce
Araahm97 <araahm97@live.no>
2019-01-25 19:41:15 +0100

    -added floating action button -added dialog for adding new elements -design still missing

--- app/src/main/java/com/example/sirwollsmoth/aranah_in2000_oblig1/ListActivity.kt
+++ app/src/main/java/com/example/sirwollsmoth/aranah_in2000_oblig1/ListActivity.kt
@@ -3,15 +3,34 @@ package com.example.sirwollsmoth.aranah_in2000_oblig1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Toast
+import com.getbase.floatingactionbutton.FloatingActionButton
+import android.content.DialogInterface
+import android.graphics.drawable.Drawable
+import android.widget.EditText
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.text.Editable
+
+
+
+
+
+
+
 
 class ListActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-    lateinit var viewAdapter: ListAdapter
+    private lateinit var viewAdapter: ListAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
-
+    private lateinit var newElementTitle: String
+    private lateinit var newElementDesc: String
+    private val elementList = ArrayList<Element>()
 
 
 
@@ -19,15 +38,21 @@ class ListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        val elementList = ArrayList<Element>()
-
 
         elementList.add(Element("ARAN", "a fat bitch"))
         elementList.add(Element("BO", "XD boi"))
         elementList.add(Element("SHIT", "spends a lot of time in toilet"))
 
+
         viewManager = LinearLayoutManager(this)
         viewAdapter = ListAdapter(elementList)
+
+        val floatingActionButton = findViewById<View>(R.id.floating_action_button) as FloatingActionButton
+
+
+        floatingActionButton.setOnClickListener({ _ -> openDialog()})
+
+
 
 
 
@@ -46,5 +71,24 @@ class ListActivity : AppCompatActivity() {
 
     }
 
+    fun openDialog() {
+        val view: View = layoutInflater.inflate(R.layout.dialog, null)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Add new Element")
+
+        val titleText: EditText = view.findViewById(R.id.element_title)
+        val descText : EditText = view.findViewById(R.id.element_desc)
+
+
+        builder.setPositiveButton("Add New") { dialog, whichButton ->
+            newElementDesc=descText.text.toString()
+            newElementTitle=titleText.text.toString()
+            elementList.add(Element(newElementTitle, newElementDesc))
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, which -> Unit }
+        builder.setView(view)
+        builder.show()
+    }
 }
 
